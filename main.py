c9514fdd960d28cfcea8500932c44dde2b27a421
wiut.tutor <wiut.tutor@gmail.com>
2021-10-15 12:38:37 +0500

    updated with comments

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+def encode(message):
+    encoded_string = ""
+    i = 0
+    while (i <= len(message)-1):
+        '''first while loop is for going through each letter'''
+        count = 1
+        ch = message[i]
+        j = i #assign current letter index to j in order to count the occurrence of letters
+        print(ch)
+        print('this is j', j)
+        print('this is i ', i)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        while (j < len(message)-1): # to loop from particular letter till the end
 
+            '''if the character at the current index is 
+            the same as the character at the next index. 
+            If the characters are the same, the count is incremented to 1'''
+            if (message[j] == message[j + 1]): #if the letters are equal to each other, e.g. A == A, keep looping, else break
+                count = count + 1
+                j = j + 1
+            else:
+                break
+        '''the count and the character is concatenated to the encoded string'''
+        encoded_string = encoded_string + str(count) + ch
+        i = j + 1
+    return encoded_string
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("bye")
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+encoded_message = encode("ABBBBCCCCCCCCAB")
+print(encoded_message)
